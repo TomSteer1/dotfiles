bf1b6953446fc19808e9b110966d901df215e5cf
Tom <tom@tomsteer.me>
2023-01-24 18:47:10 +0000

    Removed logout button

--- config/rofi/scripts/powermenu.sh
+++ config/rofi/scripts/powermenu.sh
@@ -1,1 +1,1 @@
-rofi -show p -modi p:~/.config/rofi/scripts/rofi-power-menu -lines 6 -width 20 -theme ~/.config/rofi/theme/powermenu.rasi
+rofi -show p -modi p:~/.config/rofi/scripts/rofi-power-menu -lines 5 -width 20 -theme ~/.config/rofi/theme/powermenu.rasi
