2bdcea6cde6a4e6411f847738c14146fc85cad4f
Tom <tom@tomsteer.me>
2023-09-19 16:31:00 +0100

    Dynamic postswitch script (single monitor only)

--- desktop/bspwm/scripts/monitors/monitor.sh
+++ desktop/bspwm/scripts/monitors/monitor.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-INTERNAL_MONITOR="eDP1"
-EXTERNAL_MONITOR="HDMI1"
+INTERNAL_MONITOR="eDP-1"
+EXTERNAL_MONITOR=$1
 
-if [[ -e $HOME/dotfiles/.config ]]; then
-		source $HOME/dotfiles/.config
+# If external montior not set as argument, use HDMI-1 as default
+if [ -z "$EXTERNAL_MONITOR" ]; then
+		EXTERNAL_MONITOR="HDMI-1"
 fi
-
 
 monitor_add() {
   # Move first 5 desktops to external monitor
@@ -28,14 +28,15 @@ monitor_remove() {
 		bspc desktop "$desktop" --to-monitor "$INTERNAL_MONITOR"
 	done
 
-  # delete default desktops
-  bspc desktop Desktop --remove
+	# Delete external monitor
+	bspc monitor "$EXTERNAL_MONITOR" --remove
   # reorder desktops
-  bspc monitor "$INTERNAL_MONITOR" -o 1 2 3 4 5 6 7 8 9 ♫
+  bspc monitor "$INTERNAL_MONITOR" -d 1 2 3 4 5 6 7 8 9 ♫
 }
 
 if [[ $(xrandr -q | grep "${EXTERNAL_MONITOR} connected") ]]; then
   # set xrandr rules for docked setup
+	echo "monitor connected"
 #  xrandr --output "$INTERNAL_MONITOR" --primary --mode 1920x1080 --pos 0x0 --rotate normal --output "$EXTERNAL_MONITOR" --mode 1920x1080 --pos 1920x0 --rotate normal
   if [[ $(bspc query -D -m "${EXTERNAL_MONITOR}" | wc -l) -ne 5 ]]; then
     monitor_add
@@ -54,10 +55,10 @@ killall -q polybar
 while pgrep -u $UID -x polybar > /dev/null; do sleep 2; done
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-		MONITOR=$m polybar --reload $(hostname) &
+		MONITOR=$m polybar --reload $(cat /etc/hostname) &
 		sleep 0.5
   done
 else
-	polybar --reload $(hostname) &
+	polybar --reload $(cat /etc/hostname) &
 fi
 
